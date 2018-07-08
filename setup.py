404a76719fb01eed45b8a950f2c41f0019db1747
Martin Côté <martincote@pop-os.localdomain>
2018-07-08 17:42:18 -0400

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ def read(fname):
     # read the contents of a text file
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-# allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
